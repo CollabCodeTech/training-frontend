1ca8d124fd2212c484a4b2b31028b39909603c60
Marco Bruno <marco.bruno.br@gmail.com>
2020-03-02 17:06:36 +0100

    :bug: Go back validation of form signup

--- src/containers/FormSignup/index.js
+++ src/containers/FormSignup/index.js
@@ -21,9 +21,8 @@ function FormLogin() {
         history.replace("/dashboard");
       })
       .catch(function({ response }) {
-        console.log("response", response);
-        // const { field, error } = response.body[0];
-        // setErrors({ [field]: error });
+        const { field, error } = response.body[0];
+        setErrors({ [field]: error });
       });
   }
 
