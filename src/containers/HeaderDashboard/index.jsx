d153d04dc9027c684fbed0c221ecd6546d6599ed
Luiz G Miranda <luiz.miranda@portalunimed.com.br>
2020-03-05 12:57:57 -0300

    :zap: improvement in the HeaderDashboard component

--- src/containers/HeaderDashboard/index.jsx
+++ src/containers/HeaderDashboard/index.jsx
@@ -4,11 +4,16 @@ import LogoCollab from "../../components/LogoCollab";
 
 import { Header, PhotoUser } from "./styles";
 
-export default function HeaderDashboard() {
+export default function HeaderDashboard({ photoUser }) {
   return (
     <Header>
       <LogoCollab />
-      <PhotoUser img="https://cdn.syntese.com.br/wp-content/uploads/2019/01/thumb_AleB.jpg" />
+      <PhotoUser img={photoUser} />
     </Header>
   );
 }
+
+HeaderDashboard.defaultProps = {
+  photoUser:
+    "https://webappappi-webappappislt1.azurewebsites.net/img/63703629760793-image.jpg?width=100"
+};
