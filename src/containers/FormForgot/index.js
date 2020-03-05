a0f988ba84c76cab008f345e491d1f336ab18883
brunooomelo <utibmelo@gmail.com>
2020-03-04 23:21:40 -0300

    added requestAPI in container

--- src/containers/FormForgot/index.js
+++ src/containers/FormForgot/index.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 
 import TitleCollab from "../../components/TitleCollab";
 import FieldCollab from "../../components/FieldCollab";
-import ButtonCollab from "../../components/ButtonCollab";
+import SubmitCollab from "../../components/SubmitCollab";
 
 import FormAuth from "../../containers/FormAuth";
 import LabelCollab from "../../components/LabelCollab";
 
+import AuthService from "../../services/AuthService";
+import MessageCollab from "../../components/MessageCollab";
+
 function FormForgot() {
+  const [email, setEmail] = useState("")
+  const [success, setSuccess] = useState(false)
+
+  function onChange(e) {
+    setEmail(e.target.value)
+  }
+
+  async function onSubmit(e) {
+    e.preventDefault()
+
+    return AuthService.forgot(email).then(() => setSuccess(true)).then(() => setEmail(""));
+  }
+
+
   return (
-    <FormAuth>
+    <FormAuth onSubmit={onSubmit}>
       <TitleCollab content="Esqueci minha senha" />
 
       <LabelCollab
@@ -17,9 +34,10 @@ function FormForgot() {
         warning={true}
       />
 
-      <FieldCollab content="E-mail:" htmlFor="email" id="email" />
+      <FieldCollab content="E-mail:" htmlFor="email" id="email" name="email" onChange={onChange} value={email} />
 
-      <ButtonCollab content="Enviar" to="/dashboard" />
+      <SubmitCollab content="Enviar" />
+      { success && <MessageCollab id="success" content="Por favor, verifique seu e-mail." /> }
     </FormAuth>
   );
 }
