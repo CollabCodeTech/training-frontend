562d16b6691b8becedb91e30f37bda685592ba69
Marco Bruno <marco.bruno.br@gmail.com>
2020-03-06 14:34:56 +0100

    :sparkles: Add the possibility of the create a name

--- cypress/libs/user.builder.js
+++ cypress/libs/user.builder.js
@@ -1,10 +1,18 @@
 import faker from "faker";
 
+const generateName = () => {
+  const firstName = faker.name.firstName();
+  const lastName = faker.name.lastName();
+
+  return { name: `${firstName} ${lastName}` };
+};
+
 const nameInvalid = () => ({ name: faker.internet.password(1) });
 const emailInvalid = () => ({ email: faker.lorem.word() });
 const passwordInvalid = () => ({ password: faker.internet.password(7) });
 
 export default {
+  generateName,
   nameInvalid,
   emailInvalid,
   passwordInvalid
