beeec588541d13e3ec67b547d7c50b6e13acb867
Marco Bruno <marco.bruno.br@gmail.com>
2020-03-06 13:20:13 +0100

    :sparkles: Add the possibility of the create emails invalid

--- cypress/libs/user.builder.js
+++ cypress/libs/user.builder.js
@@ -1,7 +1,9 @@
 import faker from "faker";
 
 const nameInvalid = () => ({ name: faker.internet.password(1) });
+const emailInvalid = () => ({ email: faker.lorem.word() });
 
 export default {
-  nameInvalid
+  nameInvalid,
+  emailInvalid
 };
