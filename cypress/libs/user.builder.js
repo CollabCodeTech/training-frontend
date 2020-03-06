974dd9a826753de80a6a2b310e86b825041b1d71
Marco Bruno <marco.bruno.br@gmail.com>
2020-03-06 14:45:55 +0100

    :sparkles: Add the possibility of the create a valid email

--- cypress/libs/user.builder.js
+++ cypress/libs/user.builder.js
@@ -10,10 +10,12 @@ const generateName = () => {
 const nameInvalid = () => ({ name: faker.internet.password(1) });
 const emailInvalid = () => ({ email: faker.lorem.word() });
 const passwordInvalid = () => ({ password: faker.internet.password(7) });
+const emailValid = () => ({ email: faker.internet.email() });
 
 export default {
   generateName,
   nameInvalid,
   emailInvalid,
-  passwordInvalid
+  passwordInvalid,
+  emailValid
 };
