2d5dee77438873c695ce785a13b84a0f01de4009
Empix <jotinhave@gmail.com>
2020-03-07 18:24:36 -0400

    :white_check_mark: Create test for validation of page Forgot

---
+++ cypress/integration/pages/Forgot/index.test.js
@@ -0,0 +1,28 @@
+describe("Page Forgot", function() {
+  it("Verify if exist the field E-mail", function() {
+    cy.visit("/auth/forgot");
+
+    cy.contains("E-mail:");
+    cy.get("input[name=email]");
+  });
+
+  it("Send form without filling in the inputs", function() {
+    cy.visit("/auth/forgot");
+    cy.contains("Enviar").click();
+
+    cy.contains("E-mail é obrigatório");
+  });
+
+  it("Send form with email invalid", function() {
+    cy.visit("/auth/forgot");
+    cy.get("input[name=email]").type("empix");
+    cy.contains("Enviar").click();
+    cy.contains("Preencha com email válido");
+  });
+
+  it("Send form with all fields valid", function() {
+    cy.visit("/auth/forgot");
+    cy.get("input[name=email]").type("any.email@gmail.com");
+    cy.contains("Enviar").click();
+  });
+});
