27468c70e1d904a0757087b7b274f2e104f71c2d
Luiz Gustavo Miranda <luiz.g.miranda@gmail.com>
2020-03-15 19:07:39 +0000

    :lipstick: Remove style

--- src/containers/HeaderDashboard/styles.js
+++ src/containers/HeaderDashboard/styles.js
@@ -15,31 +15,3 @@ export const Header = styled.header`
     justify-content: center;
   }
 `;
-
-export const PhotoUser = styled.div`
-  position: relative;
-  height: 40px;
-  width: 40px;
-  border-radius: 50%;
-  vertical-align: top;
-  justify-content: center;
-  align-items: center;
-  overflow: hidden;
-  text-decoration: none;
-  border: 0;
-  margin: 10px;
-
-  &::after {
-    background: url(${props => props.img}) center / cover no-repeat;
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    width: 100%;
-  }
-
-  @media (max-width: 767.98px) {
-    display: none;
-  }
-`;
