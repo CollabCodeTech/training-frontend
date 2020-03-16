702c6b61f40ce329a0d9b076ed5cbb62cce6d19a
Empix <jotinhave@gmail.com>
2020-03-16 13:11:59 -0400

    :white_check_mark: Update test to verify if email field is valid

--- cypress/integration/pages/Forgot/index.test.js
+++ cypress/integration/pages/Forgot/index.test.js
@@ -8,21 +8,25 @@ describe("Page Forgot", function() {
 
   it("Send form without filling in the email input", function() {
     cy.visit("/auth/forgot");
-    cy.contains("Enviar").click();
 
+    cy.contains("Enviar").click();
     cy.contains("E-mail é obrigatório");
   });
 
   it("Send form with email invalid", function() {
     cy.visit("/auth/forgot");
+
     cy.get("input[name=email]").type("empix");
     cy.contains("Enviar").click();
     cy.contains("Preencha com email válido");
   });
 
-  it("Send form with all fields valid", function() {
+  it("Send form with email field valid", function() {
     cy.visit("/auth/forgot");
+
     cy.get("input[name=email]").type("any.email@gmail.com");
     cy.contains("Enviar").click();
+
+    cy.get("input[name=email] + span").should("not.exist");
   });
 });
