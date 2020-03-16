1a163af277b40cd9ea48500975b435bec218488f
Marco Bruno <marco.bruno.br@gmail.com>
2020-03-16 18:41:18 +0100

    :poop: Create the SectionFake to test the Separator

---
+++ src/pages/Dashboard/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+import { _separator } from "../../styles/tools/Separator";
+
+export const SectionFake = styled.section`
+  height: 300px;
+  ${_separator};
+`;
